750a4e9b5dd52ca0fdb0ea6c0c3990e1fa25e320
Vikram Kanse <vikramkanse.nr@gmail.com>
2025-11-24 09:45:58 +0530

    fixing deployment issues

--- agile-guide/vite.config.js
+++ agile-guide/vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const isGitHubPages = process.env.VITE_APP_DEPLOY_ENV === 'github';
+const basePath = isGitHubPages ? './' : '/';
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   // Vite sets the BASE_URL environment variable from this value.
-  base: "/agile-guide/",
-})
+  base: basePath,
+});
